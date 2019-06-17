b270da04175688aab848e156ef328de74233293a
sasadangelo <sasadangelo@gmail.com>
2019-06-17 14:32:24 +0200

    Refresh code with upstream

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /**
-x * Copyright 2015 IBM Corp. All Rights Reserved.
+ * 
+ * Copyright 2015 IBM Corp. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +19,7 @@ x * Copyright 2015 IBM Corp. All Rights Reserved.
 
 var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
-var AssistantV2 = require('watson-developer-cloud/assistant/v2'); // watson sdk
+var AssistantV2 = require('ibm-watson/assistant/v2'); // watson sdk
 
 var app = express();
 
@@ -29,34 +30,20 @@ app.use(bodyParser.json());
 // Create the service wrapper
 
 var assistant = new AssistantV2({
-  version: '2018-11-08'
+  version: '2019-02-28'
 });
 
-var newContext = {
-  global : {
-    system : {
-      turn_count : 1
-    }
-  }
-};
 
 // Endpoint to be call from the client side
 app.post('/api/message', function (req, res) {
   var assistantId = process.env.ASSISTANT_ID || '<assistant-id>';
-  if (!assistantId || assistantId === '<assistant-id>>') {
+  if (!assistantId || assistantId === '<assistant-id>') {
     return res.json({
       'output': {
         'text': 'The app has not been configured with a <b>ASSISTANT_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
       }
     });
   }
-  var contextWithAcc = (req.body.context) ? req.body.context : newContext;
-
-  if (req.body.context) {
-    contextWithAcc.global.system.turn_count += 1;
-  }
-
-  //console.log(JSON.stringify(contextWithAcc, null, 2));
 
   var textIn = '';
 
@@ -67,20 +54,17 @@ app.post('/api/message', function (req, res) {
   var payload = {
     assistant_id: assistantId,
     session_id: req.body.session_id,
-    context: contextWithAcc,
     input: {
       message_type : 'text',
-      text : textIn,
-      options : {
-        return_context : true
-      }
+      text : textIn
     }
   };
 
   // Send the input to the assistant service
   assistant.message(payload, function (err, data) {
     if (err) {
-      return res.status(err.code || 500).json(err);
+      const status = (err.code  !== undefined && err.code > 0)? err.code : 500;
+      return res.status(status).json(err);
     }
 
     return res.json(data);
--- public/js/conversation.js
+++ public/js/conversation.js
@@ -234,15 +234,18 @@ var ConversationPanel = (function () {
   }
 
   function getResponse(responses, gen) {
-    var title = '';
+    var title = '', description = '';
     if (gen.hasOwnProperty('title')) {
       title = gen.title;
+    }
+    if (gen.hasOwnProperty('description')) {
+      description = '<div>' + gen.description + '</div>';
     }
     if (gen.response_type === 'image') {
       var img = '<div><img src="' + gen.source + '" width="300"></div>';
       responses.push({
         type: gen.response_type,
-        innerhtml: title + img
+        innerhtml: title + description + img
       });
     } else if (gen.response_type === 'text') {
       responses.push({
@@ -264,7 +267,7 @@ var ConversationPanel = (function () {
       var list = getOptions(gen.options, preference);
       responses.push({
         type: gen.response_type,
-        innerhtml: title + list
+        innerhtml: title + description + list
       });
     }
   }
@@ -314,15 +317,8 @@ var ConversationPanel = (function () {
   }
 
   function sendMessage(text) {
-    // Retrieve the context from the previous server response
-    var context;
-    var latestResponse = Api.getResponsePayload();
-    if (latestResponse) {
-      context = latestResponse.context;
-    }
-
     // Send the user message
-    Api.sendRequest(text, context);
+    Api.sendRequest(text);
   }
 
   // Handles the submission of input
