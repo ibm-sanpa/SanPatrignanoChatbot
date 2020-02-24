adb9adfb080048e7f6c953ac44e3456e44d737e4
sasadangelo <sasadangelo@gmail.com>
2020-02-24 18:24:39 +0100

    First commit for Dialog rework for Cereda demo

--- export_json.sh
+++ export_json.sh
@@ -1,7 +1,6 @@
 if [ ! -f ".env" ]
 then
     echo "ERROR: file .env does not exist. Create a .env file with the following parameters:"
-    echo "       ASSISTANT_URL_V1"
     echo "       ASSISTANT_URL"
     echo "       ASSISTANT_USERNAME"
     echo "       ASSISTANT_PASSWORD"
@@ -9,9 +8,9 @@ then
 fi
 
 . .env
-echo $ASSISTANT_URL_V1
+echo $ASSISTANT_URL
 echo $ASSISTANT_USERNAME
 echo $ASSISTANT_PASSWORD
-curl -H "Content-Type: application/json" -k -X get -u "$ASSISTANT_USERNAME":"$ASSISTANT_PASSWORD"  "$ASSISTANT_URL_V1?version=2018-02-16&sort=stable&export=true&include_audit=false" > training/tmp.json
-python -m json.tool training/tmp.json > training/skill-San-Patrignano-tmp.json
+curl -H "Content-Type: application/json" -k -X get -u "$ASSISTANT_USERNAME":"$ASSISTANT_PASSWORD"  "$ASSISTANT_URL?version=2018-02-16&sort=stable&export=true&include_audit=false" > training/tmp.json
+python -m json.tool training/tmp.json > training/skill-San-Patrignano.json
 rm -f training/tmp.json
